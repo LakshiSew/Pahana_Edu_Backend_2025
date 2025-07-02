2160fd621d786adf52992fa0c01a59379ce9fe1f
LakshiSew <lakshi8999@gmail.com>
2025-07-02 09:55:21 +0530

    First commit

---
+++ src/main/java/com/Pahana_Edu_Backend/PahanaEduBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Pahana_Edu_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahanaEduBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahanaEduBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Pahana_Edu_Backend/PahanaEduBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pahana_Edu_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PahanaEduBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
